bccc8a66fac9e4f3a0fd36e43e469c75e3aea209
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2023-07-02 00:34:06 +0200

    fix index

--- index.php
+++ index.php
@@ -1,11 +1,122 @@
-<!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SwimResults</title>
+    <head>
+
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>SwimResults</title>
+	<link rel="stylesheet" href="main.css?ver=2.8.0">
+ 
+ 
+   <!-- Favicon -->
+   
+   
+
+  <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+  <link rel="manifest" href="/web/20230621090542/https://swimresults.de/site.webmanifest">
+  <link rel="mask-icon" href="safari-pinned-tab.svg" color="#204988">
+  <meta name="msapplication-TileColor" content="#ffffff">
+  <meta name="msapplication-TileImage" content="/mstile-144x144.png">
+  <meta name="theme-color" content="#ffffff">
+
+
 </head>
 <body>
-    <h1>SwimResults</h1>
-</body>
-</html>
+ 
+	<div class="background"></div>
+	<div class="page-content">
+		<div class="container">
+			<span class="title">SwimResults</span>
+		</div>
+		<div class="container">
+			<h3><a href="https://github.com/SwimResults"><code>README.md</code></a>:</h3>
+			<div class="markdown">
+			<!-- <span class="subtitle">swimresults.de befindet sich momentan im Aufbau. SwimResults ist eine Weiterentwicklung der Web-App des <a href="http://erzgebirgsschwimmcup.de">Internationalen Erzgebirgsschwimmcup</a>, welche unter dem Namen <a href="http://my.erzgebirgsschwimmcup.de">myIESC</a> bekannt ist.</span> -->
+			<!-- <span>
+				<center>README.md</center>
+			</span>
+			<hr> -->
+
+			<h1>Allgemein</h1>
+<blockquote>
+<p><code>swimresults.de</code> befindet sich momentan im Aufbau.</p>
+</blockquote>
+<p>SwimResults ist eine Weiterentwicklung der Web-App des <a href="https://erzgebirgsschwimmcup.de/">Internationalen Erzgebirgsschwimmcups</a>, welche unter dem Namen <a href="https://my.erzgebirgsschwimmcup.de/about.php">myIESC</a> bekannt ist.</p>
+<h1>Kontakt</h1>
+<p>Konrad</p>
+<p>E-Mail: kontakt@swimresults.de</p>
+<p><a href="https://github.com/sponsors/konrad2002"><img src="https://img.shields.io/static/v1?label=Sponsor&amp;message=%E2%9D%A4&amp;logo=GitHub&amp;color=%23fe8e86" alt=""></a></p>
+<h1>Status</h1>
+<p>Den aktuellen Stand der Entwicklung findet man unter <a href="https://status.swimresults.de/">status.swimresults.de</a>.</p>
+<blockquote>
+<p>Der Release von SwimResults ist für Ende 2023 geplant.</p>
+</blockquote>
+<p>Technische Informationen zum Projekt können in der <a href="https://github.com/SwimResults/SwimResults#readme">SwimResults/SwimResults/README.md</a> gefunden werden.</p>
+<p><img src="images/bg_logo.jpg" alt="SwimResults"></p>
+<hr>
+<center>© Copyright 2022-2023 - SwimResults &amp; Konrad Weiß</center><!-- 
+			<span>
+
+				<h2>Allgemein</h2>
+
+				<span class="quote"><span class="code">swimresults.de</span> befindet sich momentan im Aufbau.</span>
+
+				<p>SwimResults ist eine Weiterentwicklung der Web-App des <a href="http://erzgebirgsschwimmcup.de">Internationalen Erzgebirgsschwimmcup</a>, welche unter dem Namen <a href="http://my.erzgebirgsschwimmcup.de/about.php">myIESC</a> bekannt ist.</p>
+
+				<h2>Kontakt</h2>
+				Konrad<br>
+				E-Mail: konrad@schwimmteamerzgebirge.de
+
+
+
+				<h2>Status</h2>
+				<p>
+					Den aktuellen Stand der Entwicklung findet man unter <a href="https://status.swimresults.de">status.swimresults.de</a>.
+				</p>
+
+				<p>
+					<span class="quote">Der Release von SwimResults ist für Ende 2023 geplant.</span>
+				</p>
+
+				<hr>
+				<center>&copy; 2023 - Konrad Weiß</center>
+
+			</span> -->
+			</div>
+		</div>
+		<div class="container">
+			<h1>Links</h1>
+			<div class="link-buttons">
+				<a href="https://github.com/SwimResults" class="link-button">
+					<img src="images/github.png" alt="GitHub" class="link-button-icon">
+				</a>
+				<a href="https://my.erzgebirgsschwimmcup.de/about.php" class="link-button">
+					<img src="images/myiesc.png" alt="myIESC" class="link-button-icon">
+				</a>
+				<a href="https://app.swimresults.de/" class="link-button">
+					<img src="images/sr.png" alt="SwimResults - App" class="link-button-icon">
+				</a>
+				<a href="https://instagram.com/swimresults" class="link-button">
+					<img src="images/instagram.png" alt="Instagram" class="link-button-icon">
+				</a>
+				<a href="mailto:kontakt@swimresults.de" class="link-button">
+					<img src="images/mail.png" alt="Mail" class="link-button-icon">
+				</a>
+			</div>
+		</div>
+	</div>
+	<footer>
+		<span>
+			© 2023 - logilutions.de
+		</span>
+		<div class="links">
+			<a href="impressum.html">Impressum</a>
+			<a href="kontakt.html">Kontakt</a>
+		</div>
+	</footer>
+
+
+
+</body></html>
