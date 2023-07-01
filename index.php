308aade1507bff45759f826d8ad382fac4a11f59
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2023-07-01 23:09:16 +0200

    index file + Dockerfile

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SwimResults</title>
+</head>
+<body>
+    <h1>SwimResults</h1>
+</body>
+</html>
