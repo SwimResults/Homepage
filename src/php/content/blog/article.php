27b8fe4c89ec60863c661cb3e8c3c968eb58006b
konrad2002 <konrad@schwimmteamerzgebirge.de>
2023-07-16 22:48:01 +0200

    date in article; article title style

--- src/php/content/blog/article.php
+++ src/php/content/blog/article.php
@@ -6,6 +6,19 @@
         return "images/blog/blog-1.png";
     }
 
+    function getDateString($date): string {
+        return date("d.m.Y H:i", strtotime($date));
+    }
+
+    function getPostPublishDate($post): string {
+        $date = $post["created_at"];
+        if ($post["published_at"]) {
+            $date = $post["published_at"];
+        }
+
+        return getDateString($date);
+    }
+
     function printArticleForBlogList($post): void {
         echo('<div class="post-list-tile container">');
             echo('<img class="post-image" src="'.getImgSrc($post).'" alt="post '.$post["title"].'">');
@@ -17,7 +30,8 @@
     function printArticleFullPage($post): void {
         echo('<div class="post">');
             echo('<img class="post-image" src="'.getImgSrc($post).'" alt="post '.$post["title"].'">');
-            echo('<h1 class="post-title">'.$post["title"].'</h1>');
+            echo('<h1 class="post-title title">'.$post["title"].'</h1>');
+            echo('<span class="post-date">'.getPostPublishDate($post).'</span>');
             echo('<div class="post-list-content">');
                 echo('<div>'.$post["content_html"].'</div><br><br>');
                 echo('<span>von: <b>'.$post["author"].'</b></span><br><br>');
