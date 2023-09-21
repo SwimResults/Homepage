5d11d016cf803da8c6118702d62b5e5a2423cbe1
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2023-09-21 23:11:05 +0800

    add terms of use

---
+++ src/php/views/page/terms-of-use.php
@@ -0,0 +1,32 @@
+<p>Die nachfolgenden Nutzungsbedingungen gelten für die jeweils angegebenen Seiten oder Dienste. In jedem Fall gilt, dass der Nutzer mit der Benutzung der Dienste oder der Seite(n) den Nutzungsbedingungen zustimmt. Wird den Bedingungen nicht zugestimmt, darf der jeweilige Dienst oder die jeweilige(n) Seite(n) nicht benutzt werden.</p>
+
+
+<h1>Webanwendung</h1>
+<div>
+    <i>Die folgenden Nutzungsbedingungen gelten für die Webanwendung von SwimResults (<a href="https://app.swimresults.de">app.swimresults.de</a>) im folgenden als "SwimResults Web-App" bezeichnet.</i>
+</div>
+<ol>
+    <li>Mit der Verwendung der SwimResults Web-App stimmt jeder Nutzer diesen Nutzungsbedingungen zu.</li>
+    <li>Die Informationen aus der SwimResults Web-App sind vorläufig, nicht amtlich und es können Fehler auftreten. Wir garantieren für keine Richtigkeit der angezeigten Zeiten, Meldungen, Sportlerinformationen und Ergebnisse, sowie Veranstaltungsinformationen, Dateien und Verspätungen. Ergebnisse ohne blauen Haken sind noch nicht durch das Protokoll bestätigt und nur dem Livetiming entnommen. Beanstandungen und Beschwerden auf Grundlage der SwimResults Web-App sind nicht zulässig.</li>
+    <li>Der Nutzer ist damit einverstanden, dass seine persönlichen Daten im Rahmen der <a href="/privacy-policy">Datenschutzerklärung</a> verarbeitet und gespeichert werden.</li>
+    <li>Die Nutzung der SwimResults Web-App ist freiwillig und für die Teilnahme an den Wettkampfveranstaltungen in keiner Form erforderlich.</li>
+    <li>Es ist untersagt die Daten aus SwimResults in irgendeiner Form automatisiert abzugreifen..</li>
+</ol>
+
+<br>
+<br>
+
+<h1>Account</h1>
+<div>
+    <i>Die folgenden Nutzungsbedingungen gelten für SwimResults Accounts, die über die SwimResults Webanwendung auf <a href="https://auth.swimresults.de">auth.swimresults.de</a> erstellt werden können.</i>
+</div>
+<ol>
+    <li>Mit der Registrierung eines SwimResults Accounts stimmt jeder Nutzer diesen Nutzungsbedingungen zu.</li>
+    <li>Die bei der Registrierung angegebenen Daten müssen wahrheitsgemäß ausgefüllt werden.</li>
+    <li>Jeder Nutzer sollte seine Anmeldedaten nur für sich verwenden. Die Weitergabe des Benutzernamens und des Passworts ist verboten.</li>
+    <li>Der Nutzer ist damit einverstanden, dass seine persönlichen Daten im Rahmen der <a href="/privacy-policy">Datenschutzerklärung</a> verarbeitet und gespeichert werden.</li>
+    <li>Jeder Nutzer darf nur einen Account besitzen.</li>
+    <li>Ein SwimResults-Account kann jederzeit über das Benutzermenü der SwimResults Webanwendung gelöscht werden.</li>
+    <li>Bei der Erstellung eines Accounts beziehungsweise beim Login über einen Drittdienst wie Google oder GitHub werden die dafür notwendigen Daten übertragen.</li>
+
+</ol>
