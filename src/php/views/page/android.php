7c7be7e546a40a8763044dbc94a88ebd75d29978
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2025-10-22 17:42:55 +0200

    add image to android app notice page

--- src/php/views/page/android.php
+++ src/php/views/page/android.php
@@ -1,3 +1,5 @@
+<img width="100%" src="/images/blog/blog-ios.png">
+
 <p>
     Die Android-App für SwimResults ist noch in Arbeit und wird sobald sie veröffentlich wurde unter diesem Link zur Verfügung stehen.
 </p>
